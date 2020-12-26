d39f5834fa342b1e67602aa61e6573062c67f9fa
dev-xuanphu <phuphamxuan278@gmail.com>
2020-12-26 14:05:19 +0700

    tao branch report

--- 24/js/25.js
+++
@@ -1,7 +0,0 @@
-$('.list-items .items').click(function(){
-    var apendImage = $(this).attr('data-image');
-    $(this).parent().parents('.product-gallery').find('.image-full').attr("src",apendImage)
-  })
-  $('.list-items').each(function(){
-    $(this).find('.items:first-child').click();
-  });
