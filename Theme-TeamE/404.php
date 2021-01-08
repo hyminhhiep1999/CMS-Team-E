ee5538da9e49fbe90c9c3e5bfd110c450f467e02
dev-xuanphu <phuphamxuan278@gmail.com>
2021-01-08 10:56:45 +0700

    delete theme

--- Theme-TeamE/404.php
+++
@@ -1,8 +0,0 @@
-<?php
-get_header();
-?>
-<?php get_template_part('module/17'); ?>
-
-<?php
-
-get_footer();
--- Theme-TeamE/page.php
+++
@@ -1,9 +0,0 @@
-<?php
-get_header();
-?>
-<?php get_template_part('module/9'); ?>
-<?php get_template_part('module/40'); ?>
-<?php get_template_part('module/41'); ?>
-<?php
-
-get_footer();
